149998f77cba1ce934dfac9a6f5826b1c9403bac
KeithAnderson1345 <KeithAnderson1345@gmail.com>
2021-04-22 11:44:11 -0400

    Player created Connection string established

---
+++ LegacyGT.Data/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegacyGT.Data
+{
+    public class Player
+    {
+        [Key]
+        public int PlayerId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public int Handicap { get; set; }
+
+        [Required]
+        public string ShirtSize { get; set; }
+
+        [Required]
+        public bool Dinner { get; set; }
+
+        [Required]
+        public bool Raffle { get; set; }
+
+        [Required]
+        public bool Mulligans { get; set; }
+
+        [Required]
+        public DateTimeOffset Created { get; set; }
+
+        public DateTimeOffset? Modified { get; set; }
+    }
+}
