01320b9e82dc0cbe1f99a9bbc8444c2c9ed92cbb
KeithAnderson1345 <KeithAnderson1345@gmail.com>
2021-04-22 12:03:30 -0400

    Positions created with FK to VolunteerId

---
+++ LegacyGT.Data/Position.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegacyGT.Data
+{
+    public class Position
+    {
+        [Key]
+        public int PositionId { get; set; }
+
+        [Required]
+        public string Positions { get; set; }
+
+        [ForeignKey(nameof(Volunteer))]
+        public int VolunteerId { get; set; }
+        public virtual Volunteer Volunteer { get; set; }
+
+    }
+}
